558cbb6eae9068666d760f4081cd0af03895daac
sky94520 <541052067@qq.com>
2019-08-27 22:03:24 +0800

    by xiaoniu 还是有黑边。。。

--- SDL_Engine/SEFastLayer.cpp
+++ SDL_Engine/SEFastLayer.cpp
@@ -11,39 +11,38 @@ NS_SDL_BEGIN
 
 FastLayer::FastLayer()
 	:_layerName("")
-	,_width(0)
-	,_height(0)
-	,_offsetX(0)
-	,_offsetY(0)
-	,_tiledMap(nullptr)
-	,_bufferWidth(0)
-	,_bufferHeight(0)
-	,_tileWidth(0)
-	,_tileHeight(0)
-	,_extraSize(0)
-	,_buffer(nullptr)
-	,_deltaWidth(0)
-	,_deltaHeight(0)
-	,_mapOffsetX(0)
-	,_mapOffsetY(0)
-	,_carmarkX(0)
-	,_carmarkY(0)
-	,_cmdIndex(0)
-	,_otherPosX(0)
-	,_otherPosY(0)
+	, _width(0)
+	, _height(0)
+	, _offsetX(0)
+	, _offsetY(0)
+	, _tiledMap(nullptr)
+	, _bufferWidth(0)
+	, _bufferHeight(0)
+	, _tileWidth(0)
+	, _tileHeight(0)
+	, _extraSize(0)
+	, _buffer(nullptr)
+	, _deltaWidth(0)
+	, _deltaHeight(0)
+	, _mapOffsetX(0)
+	, _mapOffsetY(0)
+	, _carmarkX(0)
+	, _carmarkY(0)
+	, _cmdIndex(0)
+	, _otherPosX(0)
+	, _otherPosY(0)
 {
 }
 
 FastLayer::~FastLayer()
 {
-	SDL_SAFE_RELEASE(_buffer);
 }
 
-FastLayer* FastLayer::create(rapidxml::xml_node<>*root)
+FastLayer* FastLayer::create(rapidxml::xml_node<>* root)
 {
 	auto layer = new FastLayer();
 
-	if(layer && layer->init(root))
+	if (layer && layer->init(root))
 		layer->autorelease();
 	else
 		SDL_SAFE_DELETE(layer);
@@ -51,11 +50,11 @@ FastLayer* FastLayer::create(rapidxml::xml_node<>*root)
 	return layer;
 }
 
-bool FastLayer::init(rapidxml::xml_node<>*root)
+bool FastLayer::init(rapidxml::xml_node<>* root)
 {
 	bool visible = true;
 	//遍历属性
-	for (auto attribute = root->first_attribute();attribute != nullptr;attribute = attribute->next_attribute())
+	for (auto attribute = root->first_attribute(); attribute != nullptr; attribute = attribute->next_attribute())
 	{
 		std::string sName = attribute->name();
 		const char* value = attribute->value();
@@ -78,11 +77,11 @@ bool FastLayer::init(rapidxml::xml_node<>*root)
 	this->setVisible(visible);
 
 	bool ret = false;
-	for(auto node = root->first_node();node;node = node->next_sibling())
+	for (auto node = root->first_node(); node; node = node->next_sibling())
 	{
-		if (SDL_strcmp(node->name(),"data") == 0)
+		if (SDL_strcmp(node->name(), "data") == 0)
 			ret |= parseData(node);
-		else if(SDL_strcmp(node->name(),"properties") == 0)
+		else if (SDL_strcmp(node->name(), "properties") == 0)
 			ret |= this->parseProperties(node);
 	}
 
@@ -91,22 +90,21 @@ bool FastLayer::init(rapidxml::xml_node<>*root)
 
 void FastLayer::draw()
 {
-	//转换成世界坐标
-	Point position = this->convertToWorldSpace(_position);
-	//只有坐标发生变化时才尝试重新绘制
-	if (position.equals(_lastPosition)) {
-		this->fastDraw(0, 0);
+	//获取尝试转换世界坐标
+	auto position = this->convertToWorldSpace(_position);
+
+	if (position.equals(_lastPosition))
+	{
+		this->fastDraw(-_otherPosX, -_otherPosY);
 		return;
 	}
 	//获取变化率
-	float dx = _lastPosition.x - position.x;
-	float dy = _lastPosition.y - position.y;
+	int dx = int(_lastPosition.x - position.x);
+	int dy = int(_lastPosition.y - position.y);
 	_lastPosition = position;
-	//部分偏移映射到地图上
-	this->fastDraw(-dx, -dy);
+	
+	this->fastDraw(-_otherPosX - dx, -_otherPosY - dy);
 
-	//TODO: 用处在哪???
-	/*
 	bool exit = false;
 	if (_otherPosX != 0 && dx != 0)
 	{
@@ -119,8 +117,9 @@ void FastLayer::draw()
 		exit = true;
 	}
 	if (exit)
+	{
 		return;
-	*/
+	}
 	//是否移动成功
 	bool ret = this->scroll(dx, dy);
 	if (!ret)
@@ -135,13 +134,13 @@ int FastLayer::getTileGIDAt(const Point& pos)const
 	int x = (int)pos.x;
 	int y = (int)pos.y;
 
-	return getTileGIDAt(x,y);
+	return getTileGIDAt(x, y);
 }
 
-int FastLayer::getTileGIDAt(int tileCoordinateX,int tileCoordinateY)const
+int FastLayer::getTileGIDAt(int tileCoordinateX, int tileCoordinateY)const
 {
-	if(tileCoordinateY >= 0 && tileCoordinateY < _height 
-		&& tileCoordinateX >=0 && tileCoordinateX < _width)
+	if (tileCoordinateY >= 0 && tileCoordinateY < _height
+		&& tileCoordinateX >= 0 && tileCoordinateX < _width)
 	{
 		int z = (int)(tileCoordinateX + tileCoordinateY * _width);
 		return _data[z];
@@ -152,14 +151,14 @@ int FastLayer::getTileGIDAt(int tileCoordinateX,int tileCoordinateY)const
 void FastLayer::removeTileAt(const Point& pos)
 {
 	int gid = this->getTileGIDAt(pos);
-	if(gid)
+	if (gid)
 	{
 		int z = (int)(pos.x + pos.y * _width);
 		//设置对应位置为空
 		_data[z] = 0;
 		//获取对应的精灵
-		Sprite*tileSprite = this->getChildByTag<Sprite*>(z);
-		if(tileSprite)
+		Sprite* tileSprite = this->getChildByTag<Sprite*>(z);
+		if (tileSprite)
 			tileSprite->removeFromParent();
 	}
 }
@@ -168,10 +167,10 @@ void FastLayer::setTileGID(int gid, const Point& pos)
 {
 	//获取对应位置的gid
 	auto oldGid = this->getTileGIDAt(pos);
-	if(oldGid == gid)
+	if (oldGid == gid)
 		return;
 	//清除掉原来的精灵
-	if(gid == 0)
+	if (gid == 0)
 	{
 		this->removeTileAt(pos);
 	}
@@ -179,7 +178,7 @@ void FastLayer::setTileGID(int gid, const Point& pos)
 	{
 		//暂时不支持
 		//新建texture
-		if(oldGid == 0)
+		if (oldGid == 0)
 		{
 			SDL_assert(SDL_FALSE);
 		}
@@ -188,7 +187,7 @@ void FastLayer::setTileGID(int gid, const Point& pos)
 			//更新data
 			int x = (int)pos.x;
 			int y = (int)pos.y;
-			int z = x +y * _width;
+			int z = x + y * _width;
 			_data[z] = gid;
 		}
 	}
@@ -205,10 +204,10 @@ bool FastLayer::initCarmark()
 	_tileWidth = (int)tileSize.width;
 	_tileHeight = (int)tileSize.height;
 	//稍微使得缓冲区大点
-	_extraSize = tileSize * 2;
+	_extraSize = tileSize;
 
 	//缓冲区要稍微比屏幕的尺寸大一些，并且能被tileSize整除
-	Size visibleSize = this->getVisibleSize();
+	Size visibleSize = Director::getInstance()->getVisibleSize();
 	int temp = 0;
 	while (temp < visibleSize.width)
 		temp += _tileWidth;
@@ -222,6 +221,7 @@ bool FastLayer::initCarmark()
 	//缓冲区图块个数
 	_bufferRowTileNum = _bufferWidth / (int)tileSize.width;
 	_bufferColTileNum = _bufferHeight / (int)tileSize.height;
+
 
 	//地图变量初始化
 	_deltaWidth = int(_width * _tileWidth - visibleSize.width);
@@ -246,10 +246,10 @@ void FastLayer::initBuffer()
 	Color4B oldColor = renderer->getDrawColor();
 	//渲染到缓冲区
 	renderer->setTarget(_buffer);
-	renderer->setDrawColor(Color4B(0,0,0,0));
+	renderer->setDrawColor(Color4B(0, 0, 0, 0));
 	renderer->clear();
 
-	auto visibleSize = this->getVisibleSize();
+	auto visibleSize = Director::getInstance()->getVisibleSize();
 	//绘制小的那个
 	int endX = _width > _bufferRowTileNum ? _bufferRowTileNum : _width;
 	int endY = _height > _bufferColTileNum ? _bufferColTileNum : _height;
@@ -269,7 +269,7 @@ void FastLayer::initBuffer()
 
 void FastLayer::fastDraw(int x, int y)
 {
-	auto visibleSize = this->getVisibleSize();
+	auto visibleSize = Director::getInstance()->getVisibleSize();
 	int visibleWidth = (int)visibleSize.width;
 	int visibleHeight = (int)visibleSize.height;
 
@@ -299,7 +299,7 @@ void FastLayer::drawRegion(int srcX, int srcY, int width, int height, int destX,
 	if (width <= 0 || height <= 0)
 		return;
 
-	auto visibleSize = this->getVisibleSize();
+	auto visibleSize = Director::getInstance()->getVisibleSize();
 	int visibleWidth = (int)visibleSize.width;
 	int visibleHeight = (int)visibleSize.height;
 	//超出屏幕检测
@@ -335,7 +335,7 @@ void FastLayer::drawTile(Renderer* renderer, int id, int destX, int destY)
 	Texture* texture = Director::getInstance()->getTextureCache()->getTextureForKey(tileset->source);
 
 	Rect srcRect = tileset->getRectForGID(id);
-	Rect destRect = Rect((float)destX, (float)destY, (float)_tileWidth, (float)_tileHeight);
+	Rect destRect = Rect(destX, destY, _tileWidth, _tileHeight);
 
 	//绘制
 	renderer->copy(texture, srcRect, destRect);
@@ -346,18 +346,18 @@ bool FastLayer::scroll(float x, float y)
 	x += _mapOffsetX;
 	y += _mapOffsetY;
 
-	if (x < 0.f || y < 0.f)
+	if (x < 0 || y < 0)
 		return false;
 
 	//缓冲区的偏移
-	if (x >= _deltaWidth)
+	if (x > _deltaWidth)
 	{
-		_mapOffsetX = (float)_deltaWidth;
+		_mapOffsetX = _deltaWidth;
 		return false;
 	}
-	if (y >= _deltaHeight)
+	if (y > _deltaHeight)
 	{
-		_mapOffsetY = (float)_deltaHeight;
+		_mapOffsetY = _deltaHeight;
 		return false;
 	}
 	//更新缓冲区
@@ -380,7 +380,6 @@ void FastLayer::updateBuffer(float x, float y)
 			copyBufferX(indexMapLastX, getIndexCarmarkY(),
 				getCarTileColNum(),
 				getBufferCarmarkX(), getBufferCarmarkY());
-
 			_carmarkX += _tileWidth;
 		}
 	}
@@ -426,7 +425,7 @@ void FastLayer::copyBufferX(int indexMapX, int indexMapY, int tileColNum, int de
 	Color4B oldColor = renderer->getDrawColor();
 	//渲染到缓冲区
 	renderer->setTarget(_buffer);
-	renderer->setDrawColor(Color4B(0,0,0,0));
+	renderer->setDrawColor(Color4B(0, 0, 0, 0));
 	//局部刷新
 	SDL_Rect rect = { destX, 0, _tileWidth, _tileHeight * _bufferColTileNum };
 	renderer->fillRect(rect);
@@ -459,7 +458,7 @@ void FastLayer::copyBufferY(int indexMapX, int indexMapY, int tileRowNum, int de
 	Color4B oldColor = renderer->getDrawColor();
 	//渲染到缓冲区
 	renderer->setTarget(_buffer);
-	renderer->setDrawColor(Color4B(0,0,0,0));
+	renderer->setDrawColor(Color4B(0, 0, 0, 0));
 
 	//局部刷新
 	//拷贝地图上面到缓冲区的下面？？
@@ -527,7 +526,7 @@ int FastLayer::getCarTileColNum() const
 }
 
 
-bool FastLayer::parseData(rapidxml::xml_node<>*root)
+bool FastLayer::parseData(rapidxml::xml_node<>* root)
 {
 	std::string encoding = root->first_attribute("encoding")->value();
 	std::string compression = root->first_attribute("compression")->value();
@@ -535,36 +534,36 @@ bool FastLayer::parseData(rapidxml::xml_node<>*root)
 	std::string decodeIDs;
 
 	//判断encoding
-	decodeIDs = this->decode(encoding,trim(data));
-	this->uncompression(compression,decodeIDs);
+	decodeIDs = this->decode(encoding, trim(data));
+	this->uncompression(compression, decodeIDs);
 
 	return true;
 }
 
-bool FastLayer::parseProperties(rapidxml::xml_node<>*root)
+bool FastLayer::parseProperties(rapidxml::xml_node<>* root)
 {
-	for(auto node = root->first_node();node;node = node->next_sibling())
+	for (auto node = root->first_node(); node; node = node->next_sibling())
 	{
 		std::string key = node->first_attribute("name")->value();
 		std::string value = node->first_attribute("value")->value();
 
-		_properties.insert(std::make_pair(key,Value(value)));
+		_properties.insert(std::make_pair(key, Value(value)));
 	}
 	return true;
 }
 
-std::string FastLayer::trim(const std::string&str)
+std::string FastLayer::trim(const std::string& str)
 {
 	//分配一个同样大小的字符数组
-	char*text = new char[str.length() + 1];
+	char* text = new char[str.length() + 1];
 
 	memset(text, '\0', sizeof(text));
 
-	int i=0;//text指针
-	for(unsigned int j=0;j < str.length();)
+	int i = 0;//text指针
+	for (unsigned int j = 0; j < str.length();)
 	{
 		auto ch = str.at(j++);
-		if(ch == '\t' || ch == '\n' || ch == '\r' || ch == ' ')
+		if (ch == '\t' || ch == '\n' || ch == '\r' || ch == ' ')
 		{
 			;
 		}
@@ -579,25 +578,25 @@ std::string FastLayer::trim(const std::string&str)
 	return trimstr;
 }
 
-std::string FastLayer::decode(const std::string&encoding,const std::string&str)
+std::string FastLayer::decode(const std::string& encoding, const std::string& str)
 {
 	std::string decodeIDs;
-	if(encoding == "base64")
+	if (encoding == "base64")
 	{
 		decodeIDs = base64_decode(str);
 	}
 	return decodeIDs;
 }
 
-void FastLayer::uncompression(const std::string&compression,const std::string&decodeIDs)
+void FastLayer::uncompression(const std::string& compression, const std::string& decodeIDs)
 {
-	if(compression == "zlib")
+	if (compression == "zlib")
 	{
-		uLongf numGids = _width * _height*sizeof(unsigned);
+		uLongf numGids = _width * _height * sizeof(unsigned);
 		auto gids = std::vector<unsigned>(numGids);
 
 		//unzip
-		uncompress((Bytef*)&gids[0],&numGids,(const Bytef*)decodeIDs.c_str(),decodeIDs.size());
+		uncompress((Bytef*)& gids[0], &numGids, (const Bytef*)decodeIDs.c_str(), decodeIDs.size());
 
 		//赋值
 		_data = std::vector<unsigned>(_width * _height);
@@ -605,14 +604,8 @@ void FastLayer::uncompression(const std::string&compression,const std::string&de
 	}
 }
 
-void FastLayer::setTiledMap(FastTiledMap*tiledMap)
+void FastLayer::setTiledMap(FastTiledMap* tiledMap)
 {
 	_tiledMap = tiledMap;
-}
-
-Size FastLayer::getVisibleSize() const
-{
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	return Size(visibleSize.width + _tileWidth, visibleSize.height + _tileHeight);
 }
 NS_SDL_END
